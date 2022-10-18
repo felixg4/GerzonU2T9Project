938bef7d6fbdcad6d5422223d04ae0719361bc0c
BT_3W2_32 <felixg10@nycstudents.net>
2022-10-18 09:31:05 -0400

    Initial commit

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the x-coordinate of the first coordinate: ");
+        double x1 = scan.nextDouble();
+        System.out.print("Enter the y-coordinate of the first coordinate: ");
+        double y1 = scan.nextDouble();
+        System.out.print("Enter the x-coordinate of the second coordinate: ");
+        double x2 = scan.nextDouble();
+        System.out.print("Enter the y-coordinate of the second coordinate: ");
+        double y2 = scan.nextDouble();
+        
+    }
+}
