44d18607064b180aafe0b5e24c801545bb8804fd
BT_3W2_32 <felixg10@nycstudents.net>
2022-10-19 09:28:42 -0400

    testing but done

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private double x1, y1, x2, y2;
+    public LinearEquation() {}
+    public LinearEquation(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public String lineInfo() {
+        String a = "Original coordinates: ("+roundHund(x1)+", "+roundHund(y1)+") and ("+roundHund(x2)+", "+roundHund(y2)+")\n";
+        a += "Line Equation: "+equation()+"\nSlope: "+roundHund(slope());
+        a += "\nY-Intercept: "+roundHund(yIntercept())+"\nDistance: "+roundHund(distance());
+        return a;
+    }
+    public double distance() {
+        return Math.sqrt(Math.pow(y2-y1, 2) + Math.pow(x2-x1, 2));
+    }
+    public double slope() {
+        return (y2-y1)/(x2-x1);
+    }
+    public double yIntercept() {
+        return y1 - slope()*x1;
+    }
+    public String equation() {
+        double m = roundHund(slope());
+        double y = roundHund(yIntercept());
+        double rise = Math.abs(roundHund(y2-y1));
+        double run = Math.abs(roundHund(x2-x1));
+        if (m == 0) {
+            String str = "y = ";
+            if (y == (int) y) return str + (int) y;
+            else return str + y;
+        }
+        String str = "y = ";
+        if (m < 0) str += "-";
+        if (rise % run == 0) {
+            rise /= run;
+            run = 1;
+        }
+        if (rise != 1) {
+            if (rise == (int) rise) str += (int) rise;
+            else str += rise;
+        }
+        str += "x";
+        System.out.println(run);
+        if (run != 1 || (rise % run != 0)) {
+            System.out.println(run!=1);
+            System.out.println(rise%run!=0);
+            str += "/";
+            if (run == (int) run) str += (int) run;
+            else str += run;
+        }
+        if (y > 0) str += " + ";
+        else str += " - ";
+        if (y == (int) y) return str + (int) Math.abs(y);
+        else return str + Math.abs(y);
+    }
+    public double coordinateForX(double x) {
+        return slope() * x + yIntercept();
+    }
+    public double roundHund(double a) {
+        return Math.round(a*100)/100.0;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
 
-public class Runner {
+public class LinearEquationRunner {
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the x-coordinate of the first coordinate: ");
-        double x1 = scan.nextDouble();
-        System.out.print("Enter the y-coordinate of the first coordinate: ");
-        double y1 = scan.nextDouble();
-        System.out.print("Enter the x-coordinate of the second coordinate: ");
-        double x2 = scan.nextDouble();
-        System.out.print("Enter the y-coordinate of the second coordinate: ");
-        double y2 = scan.nextDouble();
-        
+//        System.out.println("Hello, user!");
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter the first coordinate: ");
+//        String coord1 = scan.nextLine();
+        String coord1 = "(1,-10)";
+        int idx = coord1.indexOf(",");
+        double x1 = Double.parseDouble(coord1.substring(1, idx));
+        double y1 = Double.parseDouble(coord1.substring(idx+1, coord1.length() - 1));
+//        System.out.print("Enter the second coordinate: ");
+//        String coord2 = scan.nextLine();
+        String coord2 = "(-3,2)";
+        idx = coord2.indexOf(",");
+        double x2 = Double.parseDouble(coord2.substring(1, idx));
+        double y2 = Double.parseDouble(coord2.substring(idx + 1, coord2.length() - 1));
+        if (x1 == x2) {
+            if (y1 == y2) {
+                System.out.println("you entered the same point! A line cannot be constructed from just this information.");
+            }
+            System.out.println("That line has a infinite slope and an equation of x = "+new LinearEquation().roundHund(x1));
+        } else {
+            LinearEquation lnEq = new LinearEquation(x1, y1, x2, y2);
+            System.out.println(lnEq.lineInfo());
+//            System.out.print("enter a x value: ");
+//            double x = scan.nextDouble();
+//            double x = 0;
+//            System.out.println("The coordinate at that point is ("+lnEq.roundHund(x)+", "+lnEq.roundHund(lnEq.coordinateForX(x))+")");
+        }
     }
 }
