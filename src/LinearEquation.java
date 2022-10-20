8f80bd949e86c10940925bbc33a3dfe97e8d9785
felixg <felixg10@nycstudents.net>
2022-10-20 09:28:11 -0400

    final commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,6 +1,7 @@
 public class LinearEquation {
+    // declare instance variables
     private double x1, y1, x2, y2;
-    public LinearEquation() {}
+    // constructor
     public LinearEquation(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
@@ -8,58 +9,84 @@ public class LinearEquation {
         this.y2 = y2;
     }
     public String lineInfo() {
-        String a = "Original coordinates: ("+roundHund(x1)+", "+roundHund(y1)+") and ("+roundHund(x2)+", "+roundHund(y2)+")\n";
-        a += "Line Equation: "+equation()+"\nSlope: "+roundHund(slope());
-        a += "\nY-Intercept: "+roundHund(yIntercept())+"\nDistance: "+roundHund(distance());
+        // initialize string to rounded coordinates
+        String a = "Original coordinates: ("+roundedToHundred(x1)+", "+roundedToHundred(y1)+") and ("+roundedToHundred(x2)+", "+roundedToHundred(y2)+")\n";
+        // add slope and equation to the string
+        a += "Line Equation: "+equation()+"\nSlope: "+roundedToHundred(slope());
+        // add y-intercept and distance between points to the string
+        a += "\nY-Intercept: "+roundedToHundred(yIntercept())+"\nDistance: "+roundedToHundred(distance());
+        // return the string
         return a;
     }
+    // distance formula: sqrt( (x2-x1)^2 + (y2-y1)^2 )
     public double distance() {
         return Math.sqrt(Math.pow(y2-y1, 2) + Math.pow(x2-x1, 2));
     }
+    // slope formula: (y2 - y1)/(x2 - x1)
     public double slope() {
         return (y2-y1)/(x2-x1);
     }
+    // y intercept formula derivation:
+    // 1. y = mx+b
+    // 2. b = y - mx, m being defined by slope() and substituting y1 for y and x1 for x
     public double yIntercept() {
         return y1 - slope()*x1;
     }
+    // fasten your seatbelts for this one
     public String equation() {
-        double m = roundHund(slope());
-        double y = roundHund(yIntercept());
-        double rise = Math.abs(roundHund(y2-y1));
-        double run = Math.abs(roundHund(x2-x1));
+        // round all constants to the nearest hundredth
+        double m = roundedToHundred(slope());
+        double y = roundedToHundred(yIntercept());
+        // take the absolute value of these to add in the negative sign manually if needed.
+        double rise = Math.abs(roundedToHundred(y2-y1));
+        double run = Math.abs(roundedToHundred(x2-x1));
+        // if the slope is zero, return a string with y = y-intercept
+        // initialize te string variable
+        String str = "y = ";
         if (m == 0) {
-            String str = "y = ";
+            // if can be an integer without losing value, cast it.
             if (y == (int) y) return str + (int) y;
             else return str + y;
         }
-        String str = "y = ";
+        // if the slope is negative, put a negative sign before the coefficient
         if (m < 0) str += "-";
+        // if the slope is an integer, make the coefficient an integer
         if (rise % run == 0) {
             rise /= run;
             run = 1;
         }
+        // if the rise is 1, exclude it
         if (rise != 1) {
+            // if the rise can be an integer without losing value, cast it.
             if (rise == (int) rise) str += (int) rise;
             else str += rise;
         }
         str += "x";
-        System.out.println(run);
+        // if the slope is an integer or it is 1, exclude the denominator of the fraction
         if (run != 1 || (rise % run != 0)) {
-            System.out.println(run!=1);
-            System.out.println(rise%run!=0);
+            // add the slash
             str += "/";
+            // if the run can be an integer without losing value, cast it
             if (run == (int) run) str += (int) run;
             else str += run;
         }
+        // if the y intercept is 0, return the string as it is
+        if (y == 0) return str;
+        // if the y intercept is positive, add a plus sign before it
         if (y > 0) str += " + ";
+        // if the y intercept is negative, add a minus sign before it
         else str += " - ";
+        // if the y intercept can be an integer without losing value, cast it
+        // return the strings.
         if (y == (int) y) return str + (int) Math.abs(y);
         else return str + Math.abs(y);
     }
+    // to get an output, plug in the input by multiplying it by slope() and adding the y intercept
     public double coordinateForX(double x) {
         return slope() * x + yIntercept();
     }
-    public double roundHund(double a) {
-        return Math.round(a*100)/100.0;
+    // round to the nearest integer afer moving the decimal two places to the right, then move the decimal back.
+    public static double roundedToHundred(double toRound) {
+        return Math.round(toRound*100)/100.0;
     }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,32 +2,52 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
     public static void main(String args[]) {
-//        System.out.println("Hello, user!");
-//        Scanner scan = new Scanner(System.in);
-//        System.out.print("Enter the first coordinate: ");
-//        String coord1 = scan.nextLine();
-        String coord1 = "(1,-10)";
+        start();
+        // create scanner
+        Scanner scan = new Scanner(System.in);
+        // ask for first coordinate
+        System.out.print("Enter the first coordinate: ");
+        // store first coordinate
+        String coord1 = scan.nextLine();
+        // find where to split the coordinate
         int idx = coord1.indexOf(",");
+        // get the first part of the coordinate, from immediately after the parenthesis to immediately before the comma.
         double x1 = Double.parseDouble(coord1.substring(1, idx));
+        // get the second part of the coordinate, from immediately after the comma to immediately before the ending parenthesis.
         double y1 = Double.parseDouble(coord1.substring(idx+1, coord1.length() - 1));
-//        System.out.print("Enter the second coordinate: ");
-//        String coord2 = scan.nextLine();
-        String coord2 = "(-3,2)";
+        // ask for second coordinate
+        System.out.print("Enter the second coordinate: ");
+        // store it
+        String coord2 = scan.nextLine();
+        // find where to split it
         idx = coord2.indexOf(",");
+        // get the first part of the string (see line 14)
         double x2 = Double.parseDouble(coord2.substring(1, idx));
+        // get the second part of the string (see line 16)
         double y2 = Double.parseDouble(coord2.substring(idx + 1, coord2.length() - 1));
+        // check if both coordinates have the same x value and stop the program if yes
         if (x1 == x2) {
+            // check if both coordinates are the same and warn the user before ending the program
             if (y1 == y2) {
                 System.out.println("you entered the same point! A line cannot be constructed from just this information.");
             }
-            System.out.println("That line has a infinite slope and an equation of x = "+new LinearEquation().roundHund(x1));
-        } else {
+            // warning message for if both coordinates have the same x value but different y values.
+            System.out.println("That line has a infinite slope and an equation of x = "+ LinearEquation.roundedToHundred(x1));
+        } else { // if the coordinates are valid
+            // create a linear equation object
             LinearEquation lnEq = new LinearEquation(x1, y1, x2, y2);
+            // print the line information string
             System.out.println(lnEq.lineInfo());
-//            System.out.print("enter a x value: ");
-//            double x = scan.nextDouble();
-//            double x = 0;
-//            System.out.println("The coordinate at that point is ("+lnEq.roundHund(x)+", "+lnEq.roundHund(lnEq.coordinateForX(x))+")");
+            // ask for an x value
+            System.out.print("enter a x value: ");
+            // store it
+            double x = scan.nextDouble();
+            // print out the coordinate on the line at that x value using the LinearEquation.coordinateForX method
+            System.out.println("The coordinate at that point is ("+lnEq.roundedToHundred(x)+", "+lnEq.roundedToHundred(lnEq.coordinateForX(x))+")");
         }
+    }
+    private static void start() {
+        // greeting and brief program description
+        System.out.println("Hello, user! This program can return a linear equation and other information given two coordinate points.");
     }
 }
